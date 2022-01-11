2b56afc61029c37f7930b72d93a4cdb5b1540e48
DanyaGTx <93872886+DanyaGTx@users.noreply.github.com>
2022-01-11 19:58:14 +0200

    Update AdvancedJS1.js

--- AdvancedJS1.js
+++ AdvancedJS1.js
@@ -3,7 +3,7 @@ function solve(equation){
     let numbers = ['0','1','2','3','4','5','6','7','8','9']
     let res = '';
     let newArr = [];
-    for(let i = equation.length - 1; i>=0;i--){//Записываем всё уравнение задом на перед
+    for(let i = equation.length - 1; i>=0;i--){//Записываем всё уравнение задом наперед
         res += equation[i]
     }
     let currentNum = '';
