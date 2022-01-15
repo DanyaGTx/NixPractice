ef478175183e198de7f4810031c28d8bd0074f20
DanyaGTx <93872886+DanyaGTx@users.noreply.github.com>
2022-01-15 10:10:16 +0200

    addAdvancedJS7

---
+++ AdvancedJS7.js
@@ -0,0 +1,16 @@
+'use strict'
+function solve(s, k){
+    const str=s.split('').sort().slice(0,k)
+    const obj={};
+    str.map(v=>obj[v]=obj[v]?obj[v]+1:1)
+    s=s.split('').map((v,i,arr)=>{
+        if (obj[v]>0){
+        obj[v]--; 
+        return v='';
+        } 
+        return v;
+    })
+    return s.join('')
+}
+let func = solve('abracadabra',6)
+console.log(func)
